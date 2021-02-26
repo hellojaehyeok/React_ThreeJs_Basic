0274094a02645a152f53f386f5bcfc90d1b4e1c6
Song jae hyeok <wakij6587@naver.com>
2021-02-26 17:05:39 +0900

    박스, 빛, 카메라, 그림자, 클릭이벤트 적용
    박스, 빛, 카메라, 그림자, 클릭이벤트 적용

--- react_three/src/app.jsx
+++ react_three/src/app.jsx
@@ -1,32 +1,62 @@
-import styles from './app.module.css';
+// import styles from './app.module.css';
 
+import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from "react-three-fiber";
-import { useRef } from 'react';
+import { useSpring, a} from 'react-spring/three';
+ 
 
+const Box = ({position, args, color}) => {
+  const boxRef = useRef();
+  useFrame(() => (boxRef.current.rotation.x = boxRef.current.rotation.y += 0.01) );
 
+  const [clickBox, setClickBox] = useState(false);
+
+  const onClickBox = useSpring({scale : clickBox?[1.4, 1.4, 1.4]:[1, 1, 1] })
+
+  return(
+    <a.mesh
+      castShadow
+      onClick={() => setClickBox(!clickBox)}
+      position={position}
+      scale={onClickBox.scale}
+      ref={boxRef}>
+      <boxBufferGeometry attach='geometry' args={args}/>
+      <meshStandardMaterial attach='material' color={color} speed={1} factor={0.6} />
+    </a.mesh>
+  );
+}
 
 function App() {
 
-  const Box = () => {
-    const boxRef = useRef();
-    useFrame(() => (boxRef.current.rotation.x = boxRef.current.rotation.y += 0.01) );
-
-    return(
-      <mesh ref={boxRef}>
-      <boxBufferGeometry attach='geometry' args={[1, 1, 1]}/>
-      <meshStandardMaterial attach='material' color='lightblue'/>
-    </mesh>
-    );
-  }
-
-
-
-
   return (
     <>
-      <Canvas colorManagement camera={{ position:[-5, 2, 10], fov:60 }}>
+      <Canvas shadowMap colorManagement camera={{ position:[-5, 2, 10], fov:60 }}>
+
         <ambientLight intensity={0.3} />
-        <Box />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={[1.5]}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, 20]} intensity={1} />
+
+        <group>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
+            <planeBufferGeometry attach='geometry' args={[20, 20]}/>
+            <shadowMaterial attach='material' color='#fff'/>
+          </mesh>
+          <Box position={[0, 0, 0]} args={[1, 2, 2]} color={'lightblue'}/>
+          <Box position={[3, 0 , 0]} args={[1, 1, 1]} color={'pink'}/>
+          <Box position={[-3, 0, 0]} args={[1, 1, 1]} color={'pink'}/>
+        </group>
+
       </Canvas>
     </>
   );
