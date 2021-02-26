a813a0e8623d058ec407638d93be19dc405c0a4e
Song jae hyeok <wakij6587@naver.com>
2021-02-26 12:46:20 +0900

    기본 box, light, camera 설정
    기본 box, light, camera 설정

--- react_three/src/app.jsx
+++ react_three/src/app.jsx
@@ -1,10 +1,34 @@
 import styles from './app.module.css';
 
+import { Canvas, useFrame } from "react-three-fiber";
+import { useRef } from 'react';
+
+
+
 function App() {
+
+  const Box = () => {
+    const boxRef = useRef();
+    useFrame(() => (boxRef.current.rotation.x = boxRef.current.rotation.y += 0.01) );
+
+    return(
+      <mesh ref={boxRef}>
+      <boxBufferGeometry attach='geometry' args={[1, 1, 1]}/>
+      <meshStandardMaterial attach='material' color='lightblue'/>
+    </mesh>
+    );
+  }
+
+
+
+
   return (
-    <div className="App">
-      Hello SJH
-    </div>
+    <>
+      <Canvas colorManagement camera={{ position:[-5, 2, 10], fov:60 }}>
+        <ambientLight intensity={0.3} />
+        <Box />
+      </Canvas>
+    </>
   );
 }
 
